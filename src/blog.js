8544c8d2346874053ced1e85d1d4a7d8b19b8b61
Ilya Shestakov <shestakovsailor@gmail.com>
2019-06-30 15:35:35 +0300

    SFEC-1. Adaptive blog page is done.

--- src/blog.js
+++ src/blog.js
@@ -5,9 +5,11 @@ console.log("This is a project's blog.");
 window.onload = function(){ 
   const mobileMenuIcon = document.querySelector('.mobile-menu');
   const mobileMenuList = document.getElementById('mobileMenuList');
+  const mobileHeader = document.querySelector('.page-header');
   
   mobileMenuIcon.addEventListener('click', function() {
     mobileMenuList.classList.toggle('mobile-navigation');
-  });
+    mobileHeader.classList.toggle('page-header-margin-bottom');
+    });
 };
 
